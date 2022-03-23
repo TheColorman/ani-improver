a894e3882ef9c7d00c41c65751cef3fcf0e58656
Alexander Thoren <thoren.alex@gmail.com>
2022-03-23 09:56:26 +0100

    Added test request to AniList API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+const url = 'https://graphql.anilist.co'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query ($id: Int) {
+          Media (id: $id, type: ANIME) {
+            id
+            title {
+              romaji
+              english
+              native
+            }
+          }
+        }
+      `,
+      variables: {
+        id: "2488",
+      }
+    })
+  })
+
+  const json = await response.json()
+  res.status(200).json(json)
 }
