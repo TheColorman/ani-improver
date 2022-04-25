97e8249b48d4e2f291ea2933a889d897f5acd61a
Spurberino <elia07891@gmail.com>
2022-04-25 11:15:42 +0200

    actually made the website look like a website that we made pog

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div className="items-center justify-between flex-wrap bg-[#2B2D42] p-6 fixed w-screen">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+                <span className="font-semibold text-xl tracking-tight">Ani Improver</span>
+            </div>
+        </div>
+    )
+}
---
+++ components/Nav.tsx
@@ -0,0 +1,20 @@
+export default function Nav() {
+    return (
+        <div className="items-center justify-between flex-wrap bg-[#FBFBFB] absolute bottom-0 p-6 w-screen">
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                <li>
+                    <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Overview</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Stats</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Your lists</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Create list</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
