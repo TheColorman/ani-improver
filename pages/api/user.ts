ef79224c224c85610fd083b2506d17862d3aa8ab
Alexander Thoren <thoren.alex@gmail.com>
2022-04-29 10:33:16 +0200

    Added User API

---
+++ pages/api/user.ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const url = 'https://graphql.anilist.co'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const username = req.query.username as string | undefined
+  if (!username) {
+    res.status(400).json({ error: 'username is required' })
+    return
+  }
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query ($username: String) {
+          User(name:$username) {
+            avatar {
+              large
+              medium
+            },
+            name
+          }  
+        }
+      `,
+      variables: {
+        username: username,
+      }
+    })
+  })
+
+  const json = await response.json()
+  res.status(200).json(json.data)
+}
