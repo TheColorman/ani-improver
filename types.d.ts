a017873d24f4981a50860fbb44b3bae9581bd70e
Alexander Thoren <thoren.alex@gmail.com>
2022-04-29 14:32:49 +0200

    Added ApiOverview type

--- types.d.ts
+++ types.d.ts
@@ -12,4 +12,53 @@ type ApiUser = {
   } | null
 }
 
+type ApiOverview = {
+  SiteStatistics: {
+    anime: {
+      edges: [
+        {
+          node: {
+            count: number
+            date: number
+          }
+        }
+      ]
+    }
+  }
+  User: {
+    createdAt: number
+    statistics: {
+      anime: {
+        episodesWatched: number
+        meanScore: number
+        minutesWatched: number
+        statuses: [
+          {
+            status: {
+              count: number
+              status: "PLANNING"
+            }
+          }, {
+            status: {
+              count: number
+              status: "COMPLETED"
+            }
+          }, {
+            status: {
+              count: number
+              status: "DROPPED"
+            }
+          }, {
+            status: {
+              count: number
+              status: "CURRENT"
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+
 export type ApiUserResponse = ApiError | ApiUser
+export type ApiOverviewResponse = ApiError | ApiOverview
