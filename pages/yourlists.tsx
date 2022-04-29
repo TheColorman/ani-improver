520e7646c9c7c2a6cb5dd5436e9f0649132e0729
Alexander Thoren <thoren.alex@gmail.com>
2022-04-29 23:31:07 +0200

    Cleaned a bit

--- pages/yourlists.tsx
+++ pages/yourlists.tsx
@@ -16,9 +16,9 @@ const Home: NextPage = () => {
       </div>
       <div className='flex flex-col items-center absolute top-36 w-screen bg-[#e5ebf1]'>
         {/* Controls */}
-        <div className='w-10/12 h-36 bg-black/10 flex justify-between items-center'>
+        <div className='w-10/12 h-36 flex justify-between items-center'>
           {/* Left element */}
-          <div className='bg-black/10 h-8'>
+          <div className='h-8'>
             {/* Dropdown of lists */}
             <select name="LISTS" id="LISTS" className='p-1 px-2 h-full bg-white rounded'>
               {/*//! Dummy elements, fill in with API data */}
@@ -50,9 +50,10 @@ const Home: NextPage = () => {
           </div>
         </div>
         {/* List table */}
-        <div className='bg-blue-500/20 w-10/12 relative mt-8'>
+        <div className='bg-[#fafafa] rounded w-10/12 relative mt-8'>
           <h1 className="text-xl absolute left-4 -top-8">Completed</h1>
-          <table>
+          {/* //TODO: Replace table with MUI component (because fuck tables i hate them) */}
+          <table className='table-auto'>
             <thead>
               <tr>
                 {/* Title, Score, Progress, Type, Last updated, Last added, Start date, Completed date, Release date, Average score, Popularity */}
@@ -82,61 +83,51 @@ const Home: NextPage = () => {
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Score:</span>
                     <span className='text-gray-800'>10</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Progress:</span>
                     <span className='text-gray-800'>Completed</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Type:</span>
                     <span className='text-gray-800'>TV</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Last updated:</span>
                     <span className='text-gray-800'>1 day ago</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Last added:</span>
                     <span className='text-gray-800'>1 day ago</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Start date:</span>
                     <span className='text-gray-800'>1 day ago</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Completed date:</span>
                     <span className='text-gray-800'>1 day ago</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Release date:</span>
                     <span className='text-gray-800'>1 day ago</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Average score:</span>
                     <span className='text-gray-800'>10</span>
                   </span>
                 </td>
                 <td className='text-left'>
                   <span className='text-sm'>
-                    <span className='text-gray-600'>Popularity:</span>
                     <span className='text-gray-800'>10</span>
                   </span>
                 </td>
