d1f83d636768bbfeaf15c7d51322814794e736d3
Alexander Thoren <thoren.alex@gmail.com>
2022-05-01 22:32:38 +0200

    removed breakpoints

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Nav({ selected }: { selected: "overview" | "stats" | "yourlists" | "createlist" }) {
     return (
         <div className="items-center justify-between flex-wrap bg-[#FBFBFB] absolute bottom-0 p-6 w-screen">
-            <ul className="flex flex-col mt-4 md:flex-row md:space-x-16 md:mt-0 md:text-sm md:font-medium md:justify-center">
+            <ul className="flex flex-row space-x-16 mt-0 text-sm font-medium justify-center">
                 <li>
                     {selected == "overview" ? (
                         <p className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white hover:cursor-pointer" aria-current="page">Overview</p>
